aafd221448352bc2624ad9487d52e4766528f79b
Vinicius Gabriel <viniciusgabfarias123@gmail.com>
2022-04-27 17:52:42 -0300

    Metodos de Calculo

--- src/main/java/utils/Calculadora.java
+++ src/main/java/utils/Calculadora.java
@@ -1,6 +1,8 @@
 package utils;
 
 import java.awt.event.ActionEvent;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Calculadora extends CalculadoraForm {
 
@@ -8,11 +10,29 @@ public class Calculadora extends CalculadoraForm {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private BigDecimal toBigDecimal(String valor) {
+		try {
+			return new BigDecimal(valor).setScale(4, RoundingMode.HALF_EVEN);
+		} catch (Exception ex) {
+			return BigDecimal.ZERO;
+		}
+		
+	}
 
 	@Override
 	protected void btnCalcularClick(ActionEvent eV) {
-		// TODO Auto-generated method stub
+		BigDecimal capital = toBigDecimal(txtCapital.getText());
+		BigDecimal taxaJuros = toBigDecimal(txtTaxaJuros.getText());
+		BigDecimal periodo = toBigDecimal(txtPeriodo.getText());
 		
+		BigDecimal montante = taxaJuros.divide(new BigDecimal(100), RoundingMode.HALF_EVEN);
+		montante = montante.add(BigDecimal.ONE);
+		montante = montante.pow(periodo.intValue());
+		montante = montante.multiply(capital);
+		montante = montante.setScale(2, RoundingMode.HALF_EVEN);
+		
+		txtMontante.setText(montante.toPlainString());
 	}
 
 	@Override
